75e8af3b57f46d13f39143afb010d8c136649c46
Rafael Meurer <rafameureer@gmail.com>
2023-12-23 17:16:08 -0300

    Fix imports and add form validation in login component

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,39 +1,53 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
 import {
   FormControl,
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
+import { MessagesModule } from 'primeng/messages';
 import { PasswordModule } from 'primeng/password';
+import { ToastModule } from 'primeng/toast';
 import { AuthService } from '../../core/services/auth.service';
-import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    CommonModule,
     CardModule,
     ButtonModule,
     InputTextModule,
     PasswordModule,
     FormsModule,
     ReactiveFormsModule,
+    MessagesModule,
+    MessageModule,
+    CommonModule,
+    ToastModule,
   ],
+  providers: [MessageService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class LoginComponent implements OnInit {
   public form = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
   });
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private messageService: MessageService
+  ) {}
 
   public ngOnInit(): void {
     if (this.authService.isLoggedIn) {
@@ -42,6 +56,15 @@ export class LoginComponent implements OnInit {
   }
 
   public login(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Formulario invalido',
+      });
+      return;
+    }
     const username = this.form.get('username')?.value!;
     const password = this.form.get('password')?.value!;
     if (this.authService.login(username, password)) {
