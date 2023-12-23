2ac8d94b4b5997bf1a984aa3ef07c2779aabcb7c
Rafael Meurer <rafameureer@gmail.com>
2023-12-22 22:37:38 -0300

    Add routes for home and login components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { authGuard } from './core/guards/auth.guard';
+import { LoginComponent } from './modules/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+  },
+];
